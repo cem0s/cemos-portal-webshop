e11875cb1b06fe6232b3c0f5b7ad3a9fbd175b26
gladis26 <gladys@cemos.ph>
2017-06-21 14:44:24 +0800

    Customize header and footer for markdown template

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,16 @@
+<tr>
+    <td style="background-color: #8e8989">
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
+            <tr>
+                <td class="content-cell" align="center">
+                   {{--  {{ Illuminate\Mail\Markdown::parse($slot) }} --}}
+                   <p class="foot">Cemos Digital Production Studios</p>
+                   <p class="foot">Don't like this emails? <a href="">Unsubscribe</a></p>
+                   <p class="foot">Tel: +31070306699  Fax: +31 848673126</p>
+                   <p class="foot">Follow us for updates</p>
+                   <p class="foot"></p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
